770b98f92fb6864e4bcd0059eb33fb804b703801
jinliuli <jinliuli0305@gmail.com>
2024-09-25 17:50:04 +0900

    Crowling > 크롬드라이브+셀레니움라이브러리

---
+++ CrowlingTest/src/main/java/com/test/java/Ex03.java
@@ -0,0 +1,40 @@
+package com.test.java;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Ex03 {
+	
+	public static void main(String[] args) {
+		//Ex03.java
+		
+		//Jsoup으로 크롤링
+		try {
+			
+			String url = "http://localhost:8090/crowling/list.jsp";
+			
+			Document document = Jsoup.connect(url).get();
+			
+			Elements list = document.select("#list .item");
+			
+			for (Element item : list) {
+				
+				//System.out.println(item);
+				System.out.println(item.firstElementChild().text());
+				
+			}
+			
+			System.out.println("end");
+			
+			
+		} catch (Exception e) {
+			System.out.println("Ex03.main");
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
---
+++ CrowlingTest/src/main/java/com/test/java/Ex04.java
@@ -0,0 +1,46 @@
+package com.test.java;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Ex04 {
+	
+	public static void main(String[] args) {
+		
+		//셀레니움(자동화 테스트 도구 + 크롤링)
+		m1();
+		
+	}
+
+	private static void m1() {
+		
+		//크롬 드라이버
+		//https://googlechromelabs.github.io/chrome-for-testing/
+		//셀레니움 라이브러리
+		//https://www.selenium.dev/downloads/
+		
+		String webDriverID = "webdriver.chrome.driver";
+		String path = "C:\\class\\dev\\chromedriver.exe";
+		System.setProperty(webDriverID, path);
+		
+		WebDriver driver = new ChromeDriver();
+		
+		//String url = "https://naver.com";
+		String url = "http://localhost:8090/crowling/list.jsp";
+		driver.get(url);
+		
+		WebElement h1 = driver.findElement(By.tagName("h1"));
+		System.out.println(h1.getText());
+		
+		List<WebElement> list = driver.findElements(By.cssSelector(".item div:nth-child(1)"));
+		
+		list.forEach(item -> {
+			System.out.println(item.getText());
+		});
+		
+	}
+}

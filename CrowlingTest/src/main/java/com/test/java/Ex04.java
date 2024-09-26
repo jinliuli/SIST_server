cdf36c00a3b70617b3bbd6d62ea535fe67ab19bf
jinliuli <jinliuli0305@gmail.com>
2024-09-26 17:38:16 +0900

    Crowling_ex04_쌍용사이트 자동로그인 및 게시판제목가져오기

--- CrowlingTest/src/main/java/com/test/java/Ex04.java
+++ CrowlingTest/src/main/java/com/test/java/Ex04.java
@@ -12,7 +12,60 @@ public class Ex04 {
 	public static void main(String[] args) {
 		
 		//셀레니움(자동화 테스트 도구 + 크롤링)
-		m1();
+		//m1();
+		m2();
+		
+	}
+
+	private static void m2() {
+		
+		//http://lms1.sist.co.kr/
+		String url = "http://lms1.sist.co.kr/worknet/SLogin.asp";
+		
+		//드라이버 불러오는 고정 코드
+		String webDriverID = "webdriver.chrome.driver";
+		String path = "C:\\class\\dev\\chromedriver.exe";
+		System.setProperty(webDriverID, path);
+		WebDriver driver = new ChromeDriver();
+		
+		//get메서드로 url 가져오기
+		driver.get(url);
+		
+		//strLoginID
+		WebElement strLoginID = driver.findElement(By.id("strLoginID"));
+		strLoginID.sendKeys("김유리"); //타이핑(텍스트 입력)
+		
+		//strLoginPwd
+		WebElement strLoginPwd = driver.findElement(By.id("strLoginPwd"));
+		strLoginPwd.sendKeys("5960"); //타이핑(텍스트 입력)
+		
+		//.login-btn > input > 로그인버튼
+		WebElement btn = driver.findElement(By.cssSelector(".login-btn > input"));
+		//System.out.println(btn.getAttribute("value")); //로그인
+		btn.click();
+		
+		//#content > div > div > div > div.panel-body > div.popbtmbtn_section > div > a:nth-child(10)
+		WebElement a = driver.findElement(By.cssSelector("#content > div > div > div > div.panel-body > div.popbtmbtn_section > div > a:nth-child(10)"));
+		
+		a.click();
+		
+		//컴퓨터의 행동은 바로 이어져서 창을 열면서 찾기 때문에 못찾는거임 그래서 예외처리해줘야됨 
+		try {
+			//잠시 기다림
+			Thread.sleep(1000);
+		} catch (Exception e) {
+			System.out.println("Ex04.m2");
+			e.printStackTrace();
+		}
+		
+		//#modalContent > div > div.panel-body > div.row > table > tbody > tr > td:nth-child(2)
+		List<WebElement> list = driver.findElements(By.cssSelector("#modalContent > div > div.panel-body > div.row > table > tbody > tr > td:nth-child(2)"));
+		
+		System.out.println(list.size());
+		
+		for (WebElement td : list) {
+			System.out.println(td.getText());
+		}
 		
 	}
 
